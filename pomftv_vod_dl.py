1aff4ee53a877f5163f84ba9c1f025938960e659
I 4gor <iforgot@firemail.cc>
2024-05-30 16:24:46 -0300

    Inquirer for better VOD ID selection

--- pomftv_vod_dl.py
+++ pomftv_vod_dl.py
@@ -1,6 +1,7 @@
 from yt_dlp import YoutubeDL
 import sys
 import requests as req
+import inquirer
 
 # Append user after each
 URL_VOD = "https://pomf.tv/api/history/getuserhistory.php?user="
@@ -34,14 +35,33 @@ def fetch_vod_history(user: str):
 
 # Download vod using YoutubeDL
 def download_vod(url: str):
+    ydl_opts = {
+            "outtmpl":"%(title)s.%(ext)s" # Default config using the serverside filename. Change as you wish
+            }
     try:
-        with YoutubeDL() as ydl:
+        with YoutubeDL(ydl_opts) as ydl:
             ydl.download(url)
     except Exception:
         pass # YoutubeDL will handle that
 
+# User input
+def user_input(json_data: dict):
+        if len(json_data) > 1:
+            json_data.pop('result')
+            print("Available VODs:")
+            for key, value in json_data.items():
+                print(f"ID: {key}\nDate: {value['date']}\nStream Title: {value['stream_title']}\n")
 
+            questions = [
+                    inquirer.List('ID',
+                    message="Which ID would you like to download",
+                    choices=json_data)
+                    ]
+            answer = inquirer.prompt(questions) 
 
+            return answer['ID']
+        else:
+            return None
 
 # Main script, might change to a separated file
 if __name__ == "__main__":
@@ -52,21 +72,22 @@ if __name__ == "__main__":
         if user:
             json_data = fetch_vod_history(user)
 
-            print("Available VODs:")
-            for key, value in json_data.items():
-                if key != 'result':
-                    print(f"ID: {key}") 
-                    print(f"Date: {value['date']}")
-                    print(f"Stream Title: {value['stream_title']}\n")
 
-            vod_id = str(input("Enter a VOD ID: "))
-            if vod_id is not None:
-                try:
-                    url = "https:" + json_data[vod_id]["raw_url"]
-                    download_vod(url)
-                
-                except Exception:
-                    print("Invalid ID")
+            try:
+                vod_id = user_input(json_data)
+                if vod_id is not None:
+                    try:
+                        url = "https:" + json_data[vod_id]["raw_url"]
+                        download_vod(url)
+                    
+                    except Exception:
+                        print("Invalid ID.")
+                else:
+                    print("There's no VOD available.")
+
+
+            except:
+                pass
 
     except KeyboardInterrupt:
         print("\nExiting...")
